c3bdf0b9686348fbc533f966dd38189a6c2d30f0
Crowdbotics <team@crowdbotics.com>
2021-03-13 01:23:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nibebe_25008/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nibebe_25008;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "nibebe_25008";
+    }
+}
